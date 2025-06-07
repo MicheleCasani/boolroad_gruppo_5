8956e5a467cd36072f1902853bcb4e3e1f8852da
MicheleCasani <casanimichele@gmail.com>
2025-06-07 18:24:59 +0200

    [UPD]

--- src/componenti/Navbar.jsx
+++ src/componenti/Navbar.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='d-flex align-items-center text-white bg p-4'>
-            <h1>Bool Road</h1>
-        </div>
+        <>
+            <nav className="navbar rounded-4 shadow">
+                <div className="container text-center">
+                    <a className="navbar-brand fw-bold mx-auto text-light " href="#">Bool Road</a>
+                </div>
+            </nav>
+        </>
     )
 }
 
--- src/pages/PaginaViaggi.jsx
+++ src/pages/PaginaViaggi.jsx
@@ -13,23 +13,21 @@ const PaginaViaggi = () => {
 
 
     return (
-        <div>
-            <div className="container my-5">
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+            <div className="container text-center rounded-4 shadow p-5">
                 <Navbar />
-                <div className="row mt-5">
-                    <h1 className='mb-3'>Lista Viaggi</h1>
-                    {trips.map((trip, id) => {
-                        return (
-                            <div className="col-12 my-2" key={id}>
-                                <div className="card">
-                                    <h5>{trip.name}</h5>
-                                    <p>{trip.startDate}</p>
-                                    <p>{trip.endDate}</p>
-                                    <Link className="btn btn-primary " to={`/trips/${trip.id}`}>Dettaglio</Link>
-                                </div>
+                <div className="row mt-5 justify-content-center">
+                    <h1 className="mb-3 color ">Lista Viaggi</h1>
+                    {trips.map((trip, id) => (
+                        <div className="col-12 my-3" key={id}>
+                            <div className="card p-4 border-0">
+                                <h5 className="fw-bold">{trip.name}</h5>
+                                <p className="text-muted">Partenza {trip.startDate}</p>
+                                <p className="text-muted">Ritoro {trip.endDate}</p>
+                                <Link className="btn background-color w-100 text-light" to={`/trips/${trip.id}`}>Dettaglio</Link>
                             </div>
-                        )
-                    })}
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
